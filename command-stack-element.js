900e9531604f7dd205bb76a8a54575740f2b9efd
Derrick Schwabe <admin@nerdbusiness.com>
2016-08-21 23:12:56 -0600

    More WIP.

--- command-stack-element.js
+++ command-stack-element.js
@@ -1,10 +1,14 @@
 var _ = require('underscore'), 
     async = require('async')
 
+//Templating language: 
+var ejs = require('ejs')
+ejs.delimiter = '?' //< ex: <p><?= myVariable ?></p>
 
 //Make available custom elements v1 API: 
 require('webcomponentsjs-custom-element-v1')
 
+//Store all elements here: 
 var cse = {  entries : [] }
 
 //### Listener (blueprint for how to build the element;
@@ -15,8 +19,9 @@ cse.listen = function(elementName, callback) {
   var existingElement = _.find(this.entries, function(entry) { entry.element.name == elementName })
 
   if(!existingElement) {
-    //Establish the base element: 
-    window.customElements.define(elementName, class extends HTMLElement {
+
+    //Establish the base element:    HTMLAnchorElement
+    class newElement extends HTMLElement  {
       constructor() {
         super()
         this.name = elementName        
@@ -25,7 +30,33 @@ cse.listen = function(elementName, callback) {
           //fire the 'element-name/click' command. 
         })
       }
-    })
+      static get observedAttributes() { return ["data"] }
+
+      attributeChangedCallback(name, oldValue, newValue) {
+        // name will always be "data" due to observedAttributes
+        this.data = newValue
+        this.render()
+      }
+
+      render() {
+        //Render the element if it has a template: 
+        // if(resultingElement.template) {
+        //   resultingElement.innerHTML = ejs.render(resultingElement.template, resultingElement.data)        
+        // }
+        console.log('render is called')
+        console.log(this.data)
+        var newData = this.data
+        debugger
+      }
+
+      connectedCallback() {
+        console.log('connected')
+      }
+
+    }
+
+    window.customElements.define(elementName, newElement)
+
     baseElement = document.createElement(elementName)
 
     //Make an entry for it; add to known elements and define middleware array/stack:
@@ -53,6 +84,10 @@ cse.fireAll = function(state, callback) {
     //Run the middleware stack: 
     async.waterfall(middlewareToRun, function(err, state, resultingElement ) {
       if(err) return console.log(err)
+
+      //console.log(resultingElement.innerHTML)
+      //would like to call render here or do building stuff..
+
       //Put back the resulting element so it's changes are retained for the next loop:  
       that.entries[index].element = resultingElement
       callback(null)
