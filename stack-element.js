85363c50d46407d4c9af8ab57656dc5cff6af29d
Derrick Schwabe <admin@nerdbusiness.com>
2016-09-08 21:02:01 -0600

    Rewrite to leverage stack's existing 'stack' instead of extending the stack object itself.

--- stack-element.js
+++ stack-element.js
@@ -1,142 +1,70 @@
-var _ = require('underscore'), 
-    async = require('async')
+//Stack Element
 
 //Make available custom elements v1 API: 
 require('webcomponentsjs-custom-element-v1')
 
-//Store all elements here: 
-var cse = {  
-  entries : [], 
-  commandstack : require('command-stack') //< Store it's own commandstack too. 
-}
+var stackElement = function(stack) {
+  console.log('init stackElement...')
 
-//Base model for the element: 
-cse.element = {
-  template : '', //< set on connectedCallback to whatever is the innerHTML.
-}
-//(plugins should extend this object)
-
-//### Plugins ###: 
-//EJS Templates: 
-var ejs = require('ejs')
-ejs.delimiter = '?' //< ex: <p><?= myVariable ?></p>
-//Main render function: 
-cse.element.render = function(state) {
-  return ejs.render(this.template, { state: state })
-}
-//jQuery/cheerio: 
-
-//Browserify CSS: 
-cse.element.addStyle = function(css) {
-  require('browserify-css').createStyle(`${this.name} ${css}`)  
-}
-//###
-
-//### Listener (blueprint for how to build the element;
-//or one page of a set of blueprints as other listeners may
-//be defined)
-cse.listen = function(elementName, callback) {
-
-  //Determine if the element already entered into the stack: 
-  var existingElement = _.find(this.entries, function(entry) { return entry.element.name == elementName })
-
-  if(!existingElement) {
-
-    var element = _.clone(cse.element)
-    element.name = elementName
-
-    //Establish the base element: HTMLAnchorElement
-    class newElement extends HTMLElement  {
-      constructor() {
-        super()
-        this.addEventListener('click', e => {
-          //console.log(e)
-          console.log('clicked on ' + elementName)
-          //fire the 'element-name/click' command...
-          cse.fire(elementName + '/click')
-        })
-        this.name = elementName 
-      }
-      connectedCallback() {
-        //console.log('connected')
-        //This is how we can confirm when a given element is added to the DOM.
-        //To parse the element we need to undescape HTML entities...
-        let template = _.unescape(this.outerHTML)
-        //and then repair the EJS delimiters: 
-        while(template.indexOf("<!--?") >= 0) { template = template.replace("<!--?", "<?")}
-        while(template.indexOf("?-->") >= 0) { template = template.replace("?-->", "?>")}     
-        element.template = template
-      }
-    }
-
-    window.customElements.define(elementName, newElement)
-
-    element.DOM = document.createElement(elementName)
-
-    //Default renderer: 
-    if(!element.render) element.render = (state)=> { element.DOM.innerHTML = element.template }
-
-    //middleware : [callback]
-    //load any additional plugins here
-
-    //Make an entry for it; add to known elements and define middleware array/stack:
-    this.entries.push({ middleware : [callback], element : element })
-  } else {
-    //If the element already exists, just push the new middleware into the 
-    //existing stack: 
-    existingElement.middleware.push(callback)
+  //Setup the models: 
+  let elementProto = {
+    template : ''
   }
-}
 
+//  stack.on('/element/m-button', (state, next) => {
+  stack.on('/element/:elementName', (state, next) => {  
+    console.log('we have an element: ' + state.req.elementName)
 
+    let elementName = state.req.elementName
 
-//Fire (build the element)
-cse.fire = function(elementName, callback) {
-  var that = this
-  // this.commandstack.fire(path, this.commandstack.latestState, function(err, newState) {
-  //   //After firing the standard commandstack, run the middleware for just this element.
-  //   var targetEntry = _.findWhere(that.entries, { name: elementName })
-  //   cse.fireEnties([targetEntry], callback)    
-  // })
-}
+    //Determine if the element already entered into the stack: 
+    var existingElement = _.find(state.elements, function(entry) { return entry.element.name == elementName })
 
-//Fire all elements in parallel: 
-cse.fireAll = function(state, callback) {
-  var that = this
-  this.entries.forEach(function(entry, index, arr) {
+    if(!existingElement) {
 
-    //Seed the element's middleware with the state and element: 
-    var middlewareToRun = entry.middleware.slice(0)
+      var element = _.clone(elementProto)
+      element.name = elementName
 
-    //Pad each middleware function with a jquery: 
-    //i did some padding in the original command-stack i believe; can reference the technique there
-    //then can load jquery cheerio on each call, pre-bake the $this object
+      //Establish the base element: HTMLAnchorElement
+      class newElement extends HTMLElement  {
+        constructor() {
+          super()
+          this.addEventListener('click', e => {
+            //console.log(e)
+            console.log('clicked on ' + elementName)
+            //fire the 'element-name/click' command...
+            //cse.fire(elementName + '/click')
+          })
+          this.name = elementName 
+        }
+        connectedCallback() {
+          //console.log('connected')
+          //This is how we can confirm when a given element is added to the DOM.
+          //To parse the element we need to undescape HTML entities...
+          let template = _.unescape(this.outerHTML)
+          //and then repair the EJS delimiters: 
+          while(template.indexOf("<!--?") >= 0) { template = template.replace("<!--?", "<?")}
+          while(template.indexOf("?-->") >= 0) { template = template.replace("?-->", "?>")}     
+          element.template = template
+        }
+      }
 
-    middlewareToRun.unshift(function(next) { next(null, state, entry.element) })
+      window.customElements.define(elementName, newElement)
 
-    //Run the middleware stack: 
-    async.waterfall(middlewareToRun, function(err, state, resultingElement ) {
-      if(err) return console.log(err)
+      element.DOM = document.createElement(elementName)
 
-      //Put back the resulting element so it's changes are retained for the next loop:  
-      that.entries[index].element = resultingElement
+      //Default renderer: 
+      if(!element.render) element.render = (state)=> { element.DOM.innerHTML = element.template }
 
-      //Find existing instances of the element in the document
-      //so they can update based on the latest spec (resultingElement): 
-      var existingDOMelements = document.querySelectorAll(entry.element.name)
-      existingDOMelements.forEach(function(element){
-        //debugger
-        //Render by updating innerHTML with EJS output: 
-        //element.render(state)
-        //If element has pass attribute, we skip the rendering: 
-        if(element.hasAttribute('pass')) return
-        element.outerHTML = resultingElement.render(state)
-      })
-      console.log('rendered ' + entry.element.name)
-      callback(null)
-    })
+      //Make an entry for it; add to known elements and define middleware array/stack:
+      //this.entries.push({ middleware : [callback], element : element })      
+      state.element = element 
+    } else {
+      state.element = existingElement
+    }
+    next(null, state)    
   })
 }
 
 
-module.exports = cse
+module.exports = stackElement
