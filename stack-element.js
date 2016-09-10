0d56941e81caf939bf7558631376b20d6290174e
Derrick Schwabe <admin@nerdbusiness.com>
2016-09-10 16:39:25 -0600

    Fix bugs.

--- stack-element.js
+++ stack-element.js
@@ -29,7 +29,7 @@ var stackElement = function(stack) {
   //Expose a special route for defining elements: 
   //Ex: '/element/my-button'
   stack.on('/element/:elementName', (state, next) => {  
-    console.log('we have an element: ' + state.req.elementName)
+    //console.log('we have an element: ' + state.req.elementName)
 
     let elementName = state.req.elementName
 
@@ -97,26 +97,39 @@ var stackElement = function(stack) {
 
   stack.last((state, next) => {
     console.log('stack.last() ... attempting to run root command for each element')   
+    console.log('current command path: ')
     console.log(state.req.path) 
     //For each element, fire the command...
-    stack.lastOff() //< disable last off to prevent infinite loop.   
+    //stack.lastOff() //< disable last off to prevent infinite loop. 
+    //var originalElement = state.element 
+    //^ Keep track of this so we can re-apply it later.
     async.eachSeries(state.elements, function(element, callback) {
-      var search = state.req.path.search('/element/' + element.name)
-      if(search != -1) return callback(null)
+      //var search = state.req.path.search('/element/' + element.name)
+      //var search2 = state.req.path.search('/on/element/')
+      //console.log(state.req.path)
+      //if(search != -1 || search2 != -1) return callback(null)
+      if(state.req.path.search('/on/') != -1 || state.req.path.search('/element/') != -1
+      ) return callback(null)      
       //^ Avoids the command from being appended multiple times. 
       var command = '/element/' + element.name + '/on' + state.req.path
       state.element = element
       console.log('command: ')
       console.log(command)
+      console.log('element: ')
+      console.log(element.name)
       stack.fire(command, state, callback)
     }, function(err) { //Defer and then turn back on the stack.last feature: 
-      _.defer(() => { stack.lastOn() })
+      //_.defer(() => { stack.lastOn() })
+      //stack.lastOn()
+      //console.log(updatedState.)
+      //state.element = originalElement
       next(null, state)    
     })
   })
 
   stack.last((state, next) => {
-    console.log('render state')
+    console.log('render state... ')
+    //console.log(state.elements)
     //For each element on the stack...
     async.eachSeries(state.elements, function(element, callback) {
       //Find any instances of the element in the current DOM: 
