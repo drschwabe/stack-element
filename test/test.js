fd8aef1b702522e824f2bb364f2ce2e45208ccca
Derrick Schwabe <admin@nerdbusiness.com>
2017-04-02 08:58:52 +0900

    Create a legit browser test to test stack-element existing in a static HTML page and that the resulting stack.state is as expected.

--- test/test.js
+++ test/test.js
@@ -16,38 +16,31 @@ var server = http.createServer(
 
 console.log('http server for testing started on http://localhost:8080');
 
-// test('Stack is loaded', (t) => {
-//   t.plan(1)
-//   nightmare
-//   .on('console', function(type, msg, errorStack) {
-//     console.log(msg)
-//     if(errorStack) console.log(errorStack) 
-//   })  
-//   .goto('http://localhost:8080/test')
-//   .evaluate(() => window.stack)
-//   .then((stack) => t.ok(_.isObject(stack)))
-//   .catch((error) => console.error(error))
-// })
-
-test('A custom element existing in the DOM is recognized on page load', (t) => {
-  t.plan(1)
+test("Stack element in a static .html page", (t) => {
+  t.plan(4)
   nightmare
   .on('console', function(type, msg, errorStack) {
     console.log(msg)
     if(errorStack) console.log(errorStack) 
   })    
   .goto('http://localhost:8080/test')
+  .wait(1000)
   .end()
-  .evaluate(function() {
-
-    //window.stack.fire('hello')
-    return window.stack
+  .evaluate((done)=> {
+    stack.fire('element/init/my', (err, state) => {
+      return done(null, state)
+    })    
+  }) 
+  .then((state) => {
+    console.log('here are the elements: ')
+    console.log(state.elements)
+    t.ok(_.isObject(state), 'Stack state is initialized')
+    t.ok(state.elements[0].connected, 'Stack element connected.')
+    t.equals(state.elements[0].name, 'my-element', "Element's name is correct.")
+    t.equals(state.elements[0].template, '<my-element>This is a stack element</my-element>', "Template is good.")
   })
-  .then((stack) => {
-    console.log(stack)
-  })
+  .catch((err) => console.log(err))
 })
-
 
 
 test.onFinish(() => server.close())
