3cde9663e47e447d5636d82382d3fc313fd54d8e
Derrick Schwabe <admin@nerdbusiness.com>
2017-04-06 19:19:49 +0900

    Add more assertions to improve specificity of the expected results.

--- test/test.js
+++ test/test.js
@@ -17,7 +17,7 @@ var server = http.createServer(
 console.log('http server for testing started on http://localhost:8080');
 
 test("Stack element in a static .html page", (t) => {
-  t.plan(4)
+  t.plan(6)
   nightmare
   .on('console', function(type, msg, errorStack) {
     console.log(msg)
@@ -27,17 +27,27 @@ test("Stack element in a static .html page", (t) => {
   .wait(1000)
   .end()
   .evaluate((done)=> {
+    stack.on('/element/my-element/connected', (state, next) => {
+      state.ranMyElementConnectedCommand = true
+      console.log('my element connected')
+      next(null, state)
+    })
     stack.fire('element/init/my', (err, state) => {
-      return done(null, state)
+      console.log('fired element/init/my')
+      state.firedElementInitPrefix = true      
+      return done(null, state)      
     })    
   }) 
   .then((state) => {
     console.log('here are the elements: ')
     console.log(state.elements)
-    t.ok(_.isObject(state), 'Stack state is initialized')
-    t.ok(state.elements[0].connected, 'Stack element connected.')
+    t.equals(_.isObject(state), true, 'Stack state is initialized')
+    t.equals(state.elements[0].connected, true, 'Stack element connected.')
     t.equals(state.elements[0].name, 'my-element', "Element's name is correct.")
     t.equals(state.elements[0].template, '<my-element>This is a stack element</my-element>', "Template is good.")
+    t.equals(state.ranMyElementConnectedCommand, true, "/element/my-element/connected listener invoked" )
+    t.equals(state.firedElementInitPrefix, true, "successful fire of element/init/(prefix) and it reaches bottom of stack" )
+
   })
   .catch((err) => console.log(err))
 })
